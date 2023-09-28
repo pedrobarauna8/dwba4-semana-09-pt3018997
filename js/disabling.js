35050287f7119fd0dfc9532b80754b69abb32f90
pedrobarauna8 <81633121+pedrobarauna8@users.noreply.github.com>
2023-09-28 23:22:08 +0000

    .

--- js/disabling.js
+++ js/disabling.js
@@ -1,32 +1,20 @@
-/**
-	    * Disable right-click of mouse, F12 key, and save key combinations on page
-	    * By Arthur Gareginyan (arthurgareginyan@gmail.com)
-	    * For full source code, visit https://mycyberuniverse.com
-	    * https://mycyberuniverse.com/developing/disabling-right-clicking-by-using-javascript.html
-	    */
 	  window.onload = function() {
 	    document.addEventListener("contextmenu", function(e){
 	      e.preventDefault();
 	    }, false);
 	    document.addEventListener("keydown", function(e) {
-	    //document.onkeydown = function(e) {
-	      // "I" key
 	      if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
 	        disabledEvent(e);
 	      }
-	      // "J" key
 	      if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
 	        disabledEvent(e);
 	      }
-	      // "S" key + macOS
 	      if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
 	        disabledEvent(e);
 	      }
-	      // "U" key
 	      if (e.ctrlKey && e.keyCode == 85) {
 	        disabledEvent(e);
 	      }
-	      // "F12" key
 	      if (event.keyCode == 123) {
 	        disabledEvent(e);	
      	 }
@@ -45,12 +33,10 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
